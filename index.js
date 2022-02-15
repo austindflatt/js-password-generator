b776408f88297726044d3c056e7c9f68a538b462
Austin Flatt <austindflatt@gmail.com>
2022-02-15 09:10:11 -0500

    created DOM elements

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// DOM
+const resultElement = document.getElementById('result');
+const lengthElement = document.getElementById('length');
+const uppercaseElement = document.getElementById('uppercase');
+const lowercaseElement = document.getElementById('lowercase');
+const numbersElement = document.getElementById('numbers');
+const symbolsElement = document.getElementById('symbols');
+const generateElement = document.getElementById('generate');
+const clipboardElement = document.getElementById('clipboard');
 
 const randomFunc = {
     lower: generateRandLower,
@@ -6,7 +15,13 @@ const randomFunc = {
     symbol: generateRandSymbol
 }
 
-// Generator functions 
+// Event listeners
+generateElement.addEventListener('click', () => {
+    const length = lengthElement.value;
+    console.log(length);
+})
+
+// Generator functions | https://net-comber.com/charset.html
 
 function generateRandLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
